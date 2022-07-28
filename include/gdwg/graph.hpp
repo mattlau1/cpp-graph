c6bc1fae901b9deaf161b37749654c19d8d53725
Matthew Lau <matthew.lau78@gmail.com>
2022-07-29 00:05:28 +1000

    finished most accessors

--- include/gdwg/graph.hpp
+++ include/gdwg/graph.hpp
@@ -58,8 +58,8 @@ namespace gdwg {
 		// };
 
 		auto insert_node(N const& value) -> bool {
-			// return graph_.emplace(value, edge{})->second;
-			if (!graph_.contains(value)) {
+			// return graph_.emplace(value, edge{}).second;
+			if (!is_node(value)) {
 				return false;
 			}
 			graph_.emplace(value, edge{});
@@ -67,7 +67,7 @@ namespace gdwg {
 		};
 
 		auto insert_edge(N const& src, N const& dst, E const& weight) -> bool {
-			if (!graph_.contains(src) || !graph_.contains(dst)) {
+			if (!is_node(src) || !is_node(dst)) {
 				throw std::runtime_error("Cannot call gdwg::graph<N, E>::insert_edge when either src "
 				                         "or dst node does not exist");
 				return false;
@@ -78,21 +78,21 @@ namespace gdwg {
 		}
 
 		auto replace_node(N const& old_data, N const& new_data) -> bool {
-			if (!graph_.contains(old_data)) {
+			if (!is_node(old_data)) {
 				throw std::runtime_error("Cannot call gdwg::graph<N, E>::replace_node on a node that "
 				                         "doesn't exist");
 				return false;
 			}
-			if (!graph_.contains(new_data)) {
+			if (!is_node(new_data)) {
 				return false;
 			}
 
 			for (auto const& conn : graph_) {
-				if (conn->first != old_data) {
+				if (conn.first != old_data) {
 					// replace reference to old_data in conn with new_data
 					for (auto const& e : graph_.at(conn)) {
-						if (e->first == old_data) {
-							e->first = new_data;
+						if (e.first == old_data) {
+							e.first = new_data;
 							break;
 						}
 					}
@@ -104,6 +104,99 @@ namespace gdwg {
 					graph_.insert(std::move(item));
 				}
 			}
+		}
+
+		// TODO(): implement
+		// auto merge_replace_node(N const& old_data, N const& new_data) -> void;
+
+		auto erase_node(N const& value) -> bool {
+			if (is_node(value)) {
+				return false;
+			}
+			for (auto const& conn : graph_) {
+				for (auto const& e : conn) {
+					if (e.first == value) {
+						conn.second.erase(e);
+					}
+				}
+			}
+			return true;
+		}
+
+		auto erase_edge(N const& src, N const& dst, E const& weight) -> bool {
+			if (!is_node(src) || !is_node(dst)) {
+				throw std::runtime_error("Cannot call gdwg::graph<N, E>::erase_edge on src or dst if "
+				                         "they don't exist in the graph");
+				return false;
+			}
+			for (auto const& conn : graph_.at(src)) {
+				if (conn.first == dst && conn.second == weight) {
+					graph_.at(src).erase(conn);
+					break;
+				}
+			}
+			return true;
+		}
+
+		// TODO(): implement
+		// auto erase_edge(iterator i) -> iterator;
+		// auto erase_edge(iterator i, iterator s) -> iterator;
+
+		auto clear() noexcept -> void {
+			graph_.clear();
+		}
+
+		[[nodiscard]] auto is_node(N const& value) -> bool {
+			return graph_.contains(value);
+		}
+
+		[[nodiscard]] auto empty() -> bool {
+			return graph_.empty();
+		}
+
+		[[nodiscard]] auto is_connected(N const& src, N const& dst) -> bool {
+			return static_cast<bool>(std::find_if(graph_.at(src).begin(),
+			                                      graph_.at(src).end(),
+			                                      [&dst](auto const& e) { return e.first == dst; }));
+		}
+
+		[[nodiscard]] auto nodes() -> std::vector<N> {
+			auto res = std::vector<N>{};
+			res.reserve(graph_.size());
+			for (auto const& kv : graph_) {
+				res.push_back(kv.first);
+			}
+			res.sort();
+			return res;
+		}
+
+		[[nodiscard]] auto weights(N const& src, N const& dst) -> std::vector<E> {
+			if (!is_node(src) || !is_node(dst)) {
+				throw std::runtime_error("Cannot call gdwg::graph<N, E>::weights if src or dst node "
+				                         "don't exist in the graph");
+				return nullptr;
+			}
+			auto res = std::vector<E>{};
+			for (auto const& conn : graph_.at(src)) {
+				if (conn.first == dst) {
+					res.push_back(conn.second);
+				}
+			}
+			res.sort();
+			return res;
+		}
+
+		// TODO(): implement
+		// [[nodiscard]] auto find(N const& src, N const& dst, E const& weight) -> iterator;
+
+		[[nodiscard]] auto connections(N const& src) -> std::vector<N> {
+			auto res = std::vector<N>{};
+			for (auto const& conn : graph_.at(src)) {
+				res.push_back(conn.first);
+			}
+			// TODO(): sort in asc order with respect to connected nodes
+			res.sort();
+			return res;
 		}
 	};
 
